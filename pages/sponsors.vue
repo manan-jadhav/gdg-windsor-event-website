d36fe36fc33c4e5d6a4504366527e9373131f572
donwardpeng <don.ward.peng@gmail.com>
2019-07-08 09:30:19 -0400

    Added MobiStream Solutions as a sponsor

--- pages/sponsors.vue
+++ pages/sponsors.vue
@@ -37,6 +37,7 @@
   import uofw from '../static/uofw.png'
   import coulter from '../static/coulter_software.jpg'
   import wetech from '../static/wetechalliance.png'
+  import mobistream from '../static/mobistream.jpeg'
 
 export default {
     data: () => ({
@@ -44,13 +45,14 @@ export default {
         {
           name: 'The University of Windsor',
           image: uofw,
-          description: 'The University of Windsor is a comprehensive, student-focused university, with more than 15,000 students enrolled in a broad range of undergraduate and graduate programs, including: <br/> <br/> <ul> <li> Law </li> <li> Business </li> <li> Engineering </li> <li> Science </li> <li> Education </li> <li> Nursing </li> <li> Human Kinetics </li> <li> Social Work </li> </ul> <br/> We have strong student-faculty relationships, exceptional award-winning faculty and innovative staff. It creates a unifying atmosphere of excellence across all of our faculties to encourage lifelong learning, teaching, research and discovery.',
+          description: 'The Universtity of Windsor has graciously offered to host the 2nd Windsor-Essex DevFest!<p><p>The University of Windsor is a comprehensive, student-focused university, with more than 15,000 students enrolled in a broad range of undergraduate and graduate programs. <p> The University has strong student-faculty relationships, exceptional award-winning faculty and innovative staff. It creates a unifying atmosphere of excellence across all of the faculties to encourage lifelong learning, teaching, research and discovery.',
           socials: {
             facebook: 'https://www.facebook.com/UWindsor/',
             twitter: 'https://twitter.com/uwindsor',
             // github: 'https://github.com/donwardpeng',
-            linkedin: 'https://www.linkedin.com/groups/38761'
-          }
+            linkedin: 'https://www.linkedin.com/groups/38761',
+            
+          },
         },
         {
           name: 'Coulter Software',
@@ -64,6 +66,15 @@ export default {
           }
         },
         {
+          name: 'MobiStream Solutions',
+          image: mobistream,
+          description: 'We are a team of global software development professionals who are passionate about changing the way our clients do business, by building and implementing cutting-edge web and mobile technology. <p><p>We have been in the mobility business since the beginning. We love what we do and look forward to partnering with our clients in achieving innovative business solutions that drive your business.<p><p>Learn more at: <a href="http://www.mobistreamsolutions.com/">www.mobistreamsolutions.com/</a>',
+          socials: {
+            twitter: 'https://twitter.com/MobiStreamSln',
+            linkedin: 'https://www.linkedin.com/company/mobistream-solutions',
+          }
+        },
+        {
           name: 'WEtech Alliance',
           image: wetech,
           description: 'At WEtech Alliance, we want to see more tech companies start and scale in Windsor-Essex and Chatham-Kent! <p><p>Supporting entrepreneurs as they build and grow tech and tech-enabled products & companies is at the heart of WEtech Alliance. We know that technology touches every aspect of our lives; and Windsor-Essex and Chatham-Kent is packed with bright, ambitious, and hard-working entrepreneurs that are tackling challenges every day with the incredible opportunities that technology brings.<p><p>We help in three ways: TECH ACCELERATION, TECH TALENT & TECH COMMUNITY.<p><p>Since 2011, we have brought together industry, entrepreneurs, tech talent, students, founders, mentors, and investors to build a region that supports innovative businesses from Windsor-Essex and Chatham-Kent and around the world to grow locally and scale globally.<p><p>Learn more at: <a href="www.wetech-alliance.com">www.wetech-alliance.com</a>',
@@ -73,6 +84,8 @@ export default {
             linkedin: 'https://www.linkedin.com/company/2252698/',
           }
         },
+       
+
       ]
     })
   }
