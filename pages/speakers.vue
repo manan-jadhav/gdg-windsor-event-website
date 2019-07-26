2ebfa9afada5436668531eabd71cd07275935a4d
Manan Jadhav <curosmj@gmail.com>
2019-07-26 15:00:56 -0400

    update speaker and team layout

--- pages/speakers.vue
+++ pages/speakers.vue
@@ -4,21 +4,31 @@
 
     <v-container grid-list-md>
       <v-layout row wrap>
-        <v-flex xs12 md6 v-for="(speaker, i) in speakers" :key="i">
-          <v-card>
-            <v-img height="300px" :src="speaker.image"></v-img>
-            <v-card-text>
-              <p class="display-1">
-                <span v-html="speaker.name"></span>
-              </p>
-              <p class="title">
-                <span v-html="speaker.title"></span>
-              </p>
-              <p class="body-1">
-                <span v-html="speaker.description"></span>
-              </p>
-            </v-card-text>
-            <v-card-actions>
+        <v-flex xs12 md12 v-for="(speaker, i) in speakers" :key="i">
+          <v-card class="mb-4">
+            <v-layout>
+                <v-flex xs3>
+                  <v-img
+                    :src="speaker.image"
+                    contain
+                  ></v-img>
+                </v-flex>
+                <v-flex xs9>
+                  <v-card-text>
+                    <p class="display-1">
+                      <span v-html="speaker.name"></span>
+                    </p>
+                    <p class="title">
+                      <span v-html="speaker.title"></span>
+                    </p>
+                    <p class="body-1">
+                      <span v-html="speaker.description"></span>
+                    </p>
+                  </v-card-text>
+                </v-flex>
+            </v-layout>
+            <v-divider light></v-divider>
+            <v-card-actions class="pa-3">
               <v-btn
                 v-for="(link, platform) in speaker.socials"
                 :key="link"
