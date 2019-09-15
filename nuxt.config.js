b493c8e4192248bedd5f31d0ce10d35c5fae99ef
Manan Jadhav <curosmj@gmail.com>
2019-09-15 15:22:31 -0400

    update meta description

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: 'Buy your tickets now!' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
